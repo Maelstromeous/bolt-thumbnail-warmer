e1e73898f73d137063f27b59619e1462a7720d77
Matt 'Newb' Cavanagh <matt.cavanagh@barnsley-chronicle.co.uk>
2016-12-28 13:56:38 +0000

    Continued progress with Bolt Thumbnail Warmer. WIP still.

--- README.md
+++ README.md
@@ -1,10 +1,34 @@
-Bolt Extension Starter
-======================
+# Bolt Thumbnail Cache / Warmer Extension
 
-A starter skeleton for a Bolt v3.x Extension
+## Requirements
+* CURL (chances are you have this installed already)
 
-To get going run the following command, replacing the last argument with the name of your extension:
+## Usage
 
-`composer create-project --no-install bolt/bolt-extension-starter:^3.0 <newextname>`  
+Edit your `contenttypes.yml` file on any `image` type using the below as a guide:
+```
+image:
+    type: image
+    upload: test
+    cache:
+        aliases: ['some_alias', 'some_other_alias']
+        sizes: [ [900, 500], [500, 300] ]
+```
+Note the new `cache` property.
 
-For more information, see this page in the Bolt documentation: https://docs.bolt.cm/extensions/building-starter/about
+## Using aliases
+As of Bolt 3.2, you're now able to define sizes using Aliases. For more information, please see the Bolt documentation on the matter: https://docs.bolt.cm/3.2/configuration/thumbnails#thumbnail-aliases
+
+This extension will simply use the information defined in the aliases and pre-cache them on your behalf.
+
+## Using sizes
+If you don't wish to use aliases, then you're able to define the sizes yourself.
+
+E.g:
+```
+cache:
+    sizes: [ [1000, 500] ]
+```
+Will generate a single thumbnail for your image at the size of 1000x500px.
+
+You can provide multiple arrays to generate a number of thumbnails at various resolutions.
--- src/ThumbnailWarmerExtension.php
+++ src/ThumbnailWarmerExtension.php
@@ -5,43 +5,157 @@ namespace Bolt\Extension\Maelstromeous\ThumbnailWarmer;
 use Bolt\Events\StorageEvent;
 use Bolt\Events\StorageEvents;
 use Bolt\Extension\SimpleExtension;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Promise;
 use Silex\Application;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
- * Local Extension allowing for more custom functionality.
+ * ThumbnailWarmer Extension for pre-caching / warming thumbnails
  *
  * @author Matt Cavanagh <maelstrome26@gmail.com>
  */
 class ThumbnailWarmerExtension extends SimpleExtension
 {
+    protected $basePath;
+    protected $pathsToHit = [];
+
     public static function getSubscribedEvents()
     {
         $parent = parent::getSubscribedEvents();
         $events = [
             StorageEvents::POST_SAVE => [
-                ['process', 0]
+                ['check', 0]
             ]
         ];
 
         return $parent + $events;
     }
 
-    public function process(StorageEvent $event)
+    /**
+     * Checks appropiate configs and the upload event for paths to hit for generation.
+     *
+     * @param  Bolt\Events\StorageEvent $event
+     *
+     * @return void
+     */
+    public function check(StorageEvent $event)
     {
         $app = $this->getContainer();
+
+        // Ensure we have the root path so we can build our thumbnail requests
+        $this->basePath = $app['resources']->getUrl('hosturl');
+
+        // Get ContentType info
         $type = $event->getContenttype();
         $contentType = $app['config']->get("contenttypes/{$type}");
-
         $fields = $contentType['fields'];
-        var_dump($fields);
+
+        // Get upload info
+        $data = $event->getSubject()->_fields;
+
         // Check to see if we have any images
-        foreach ($fields as $key => $field) {
-            if ($field['type'] === 'image' && (! empty($field['cache']))) {
-                var_dump($field['cache']);
-                var_dump("found!");
+        foreach ($fields as $key => $image) {
+            if ($image['type'] === 'image' && (! empty($image['cache']))) {
+                // Check if we have a reference in the event. If not, abort.
+                if (empty($data[$key])) {
+                    return false;
+                }
+
+                // Get the filepath we'll need to run the processing on
+                $file = $data[$key]['file'];
+
+                var_dump($image);
+                var_dump($file);
+
+                if (! empty($image['cache']['aliases'])) {
+                    $this->processAliases($image, $file);
+                }
+                if (! empty($image['cache']['sizes'])) {
+                    $this->processSizes($image, $file);
+                }
             }
         }
-        var_dump($fields);die;
+
+        if (! empty($this->pathsToHit)) {
+            $this->hitPaths();
+        }
+        die;
+    }
+
+    /**
+     * Reviews all aliases and builds paths if required
+     *
+     * @param  array  $image Image descriptior
+     * @param  string $file  Image path
+     * @return void
+     */
+    public function processAliases($image, $file)
+    {
+        if (count($image) === 0) {
+            return false;
+        }
+
+        $app = $this->getContainer();
+
+        foreach ($image['cache']['aliases'] as $alias) {
+            // Check for the existance of the alias record
+            if (empty($app['thumbnails.aliases'][$alias])) {
+                $app['session']->getFlashBag()->set('error', "Invalid cache alias configuration for: {$alias}");
+                continue;
+            }
+
+            // Build the URL and add it to the hit array
+            $this->pathsToHit[] = $this->basePath . "/thumbs/{$alias}/{$file}";
+        }
+    }
+
+    /**
+     * Reviews all sizes and builds paths if required
+     *
+     * @param  [type] $image [description]
+     * @param  [type] $file  [description]
+     *
+     * @return [type]        [description]
+     */
+    public function processSizes($image, $file)
+    {
+        if (count($image) === 0) {
+            return false;
+        }
+        var_dump('sizes');
+    }
+
+    /**
+     * Hits the paths requires to generate the thumbnails
+     *
+     * @return boolean
+     */
+    public function hitPaths()
+    {
+        $client = $this->getContainer()['guzzle.client'];
+
+        $promises = [];
+
+        // Build Async request with Guzzle
+        foreach ($this->pathsToHit as $key => $path) {
+            $promises[] = $client->getAsync($path);
+        }
+
+        // Trigger the paths and generate the thumbs
+        try {
+            $results = Promise\unwrap($promises);
+            $this->getContainer()['session']->getFlashBag()->set(
+                'success',
+                'Thumbnails were successfully cached.'
+            );
+        } catch (RequestException $e) {
+            $this->getContainer()['session']->getFlashBag()->set(
+                'error',
+                "There was an error in caching your thumbnails. Error reported was: {$e->getMessage()}"
+            );
+        }
+
+        var_dump($results);
     }
 }
